6dd8651b80c82737496fabbe402d383e9286d7d7
David Lord <davidism@gmail.com>
2019-08-22 13:42:50 -0700

    switch to setup.cfg

--- setup.py
+++ setup.py
@@ -1,36 +1,3 @@
-#!/usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='Flask-Alembic',
-    version='2.0.1',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    url='https://bitbucket.org/davidism/flask-alembic',
-    license='BSD',
-    author='David Lord',
-    author_email='davidism@gmail.com',
-    description='Flask extension to integrate Alembic migrations',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Flask',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Database :: Front-Ends',
-    ],
-    install_requires=[
-        'alembic>=0.8',
-        'Flask>=0.10',
-        'Flask-SQLAlchemy',
-        'SQLAlchemy',
-    ],
-)
+setup()
